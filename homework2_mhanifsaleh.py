d242a02c3df705a8365e9c749f6d7a8853c0e43b
Ayub <ayubhanif06@gmail.com>
2025-05-06 02:14:42 -0700

    finished the 6th one I did alot of testing since this was harder than other ones. and I just also finished the 7th to add it and then check it

--- homework2_mhanifsaleh.py
+++ homework2_mhanifsaleh.py
@@ -9,6 +9,7 @@ student_name = "Mohammad Ayub Hanif Saleh"
 ############################################################
 
 # Include your imports here, if any are used.
+import itertools
 
 
 
@@ -33,16 +34,19 @@ class Atom(Expr):
             return self.name == other.name
         return False
     
+    #I tired my best to make it look like the expression given in the example but can't be exact.
     def __repr__(self):
         return f"Atom({self.name})"
     
     def atom_names(self):
         return {self.name}
     
+    #I think the easy was is to just check if its true or false by checking the name of the atom.
     def evaluate(self, assignment):
-        pass
+        return bool(assignment[self.name])
+    
     def to_cnf(self):
-        pass
+        return self
 
 class Not(Expr):
     def __init__(self, arg):
@@ -63,10 +67,37 @@ class Not(Expr):
     def atom_names(self):
         return self.arg.atom_names()
     
+    #The way this should be is that not of arg with the assignment.
     def evaluate(self, assignment):
-        pass
+        return not self.arg.evaluate(assignment)
+    
     def to_cnf(self):
-        pass
+        if isinstance(self.arg, Atom):
+            return self
+        if isinstance(self.arg, Not):
+            return self.arg.arg.to_cnf()
+        
+        # I think de Morgan's law we should use here so we can convert the negation of a conjunction
+        # into a disjun of neg. but we also need to check if the arg is a disjun and conjuction.
+        if isinstance(self.arg, And):
+            neg_cons = []
+            for con in self.arg.conjuncts:
+                neg_con = Not(con).to_cnf()
+                neg_cons.append(neg_con)
+            result = Or(*neg_cons)
+            return result.to_cnf()
+        
+        #same thing from above but with or.
+        if isinstance(self.arg, Or):
+            neg_dis = []
+            
+            for disjunct in self.arg.disjuncts:
+                dis = Not(disjunct).to_cnf()
+                neg_dis.append(dis)
+            result = And(*neg_dis)
+            return result.to_cnf()
+        return Not(self.arg.to_cnf()).to_cnf()
+
 
 class And(Expr):
     def __init__(self, *conjuncts):
@@ -83,16 +114,32 @@ class And(Expr):
 
     def __repr__(self):
         return f"And({before.join(repr(a) for a in self.conjuncts)})"
+    
     def atom_names(self):
         name = set()
         for con in self.conjuncts:
             name.update(con.atom_names())
         return name
     
+    #I think the easy was is to just check if its true or false by checking the name of the atom.
     def evaluate(self, assignment):
-        pass
+        for con in self.conjuncts:
+            if not con.evaluate(assignment):
+                return False
+        return True
+    
     def to_cnf(self):
-        pass
+        #temp_expr_cnf of the conjuction is conjuction of the temp_expr_cnf parts flatten nested Ands
+        cnf_conjuncts_part = []
+        for con in self.conjuncts:
+            sub = con.to_cnf()
+            if isinstance(sub, And):
+                cnf_conjuncts_part.extend(sub.conjuncts)
+            else:
+                cnf_conjuncts_part.append(sub)
+        if len(cnf_conjuncts_part) == 1:
+            return cnf_conjuncts_part[0]
+        return And(*cnf_conjuncts_part)
 
 class Or(Expr):
     def __init__(self, *disjuncts):
@@ -116,10 +163,41 @@ class Or(Expr):
             name.update(dis.atom_names())
         return name
     
+    # I think we evaluate the disjuncts and if any one of them is true then return it.
     def evaluate(self, assignment):
-        pass
+        for dis in self.disjuncts:
+            if dis.evaluate(assignment):
+                return True
+        return False
+
     def to_cnf(self):
-        pass
+        #This is same ish like last one I did I am guessing we do temp_expr_cnf of the disjunction which should be disjunction of the temp_expr_cnf parts flatten nested Or
+        # each disjunct into temp_expr_cnf first and then flatten nested Or
+        parts = [d.to_cnf() for d in self.disjuncts]
+
+        # if repeatedly distribute OR over AND until no AND is inside then we can return the result.
+        result = parts[0]
+        for i in range(1, len(parts)):
+            current_expression = parts[i]
+            result = Or._distribute(result, current_expression)
+
+        #I think making flatten nested Or that may remain after dis is done.
+        if isinstance(result, Or):
+            flat = []
+            for d in result.disjuncts:
+                flat.extend(d.disjuncts if isinstance(d, Or) else [d])
+            result = Or(*flat)
+        return result
+
+    #I know the @saticmethod from my research project once again.
+    @staticmethod # staticmethod I created so that I can call it in the to_cnf function.
+    # it makes my code cleaner and easier to read.
+    def _distribute(a, b):
+        if isinstance(a, And):
+            return And(*[Or._distribute(c, b) for c in a.conjuncts]).to_cnf()
+        if isinstance(b, And):
+            return And(*[Or._distribute(a, c) for c in b.conjuncts]).to_cnf()
+        return Or(a, b)
 
 class Implies(Expr):
     def __init__(self, left, right):
@@ -142,11 +220,18 @@ class Implies(Expr):
     def atom_names(self):
         #union of the both sides of the implies by using union function that I learned the union function when using it for my research project.
         return self.left.atom_names().union(self.right.atom_names())
-
+    
+    #the same as the or but we need to check the left and if it is false or right side bc we just do a or.
     def evaluate(self, assignment):
-        pass
+        not_eval = not self.left.evaluate(assignment)
+        eval = self.right.evaluate(assignment)
+        return not_eval or eval
+
     def to_cnf(self):
-        pass
+        first = Not(self.left).to_cnf()
+        second = self.right.to_cnf()
+        final = Or(first, second).to_cnf()
+        return final
 
 class Iff(Expr):
     def __init__(self, left, right):
@@ -158,7 +243,7 @@ class Iff(Expr):
 
     #I think this one is the same as the last eq function but this is insensitive.
     def __eq__(self, other):
-        if isinstance(self, other):
+        if isinstance(Iff, other):
             return {self.left, self.right} == {other.left, other.right}
         return False
 
@@ -168,23 +253,87 @@ class Iff(Expr):
     def atom_names(self):
         return self.left.atom_names().union(self.right.atom_names())
 
+    #iff is true if both sides are false or true otherwise it is false.
     def evaluate(self, assignment):
-        pass
+        left_eval = self.left.evaluate(assignment)
+        right_eval = self.right.evaluate(assignment)
+        return left_eval == right_eval
+
     def to_cnf(self):
-        pass
+        A_to_B = Implies(self.left, self.right).to_cnf()
+        B_to_A = Implies(self.right, self.left).to_cnf()
+        final = And(A_to_B, B_to_A).to_cnf()
+        return final
 
 def satisfying_assignments(expr):
-    pass
-
+    name = expr.atom_names()
+    #changed the F and T so it matches the example but IDK if they care alot? or want it either way.
+    for value in itertools.product([False, True], repeat=len(name)):
+        #we make a dict of the name and value and then we check if the expr is true or false.
+        #the zip func is used to bring name and value tog to make it easier to check the expr.
+        temp = dict(zip(name, value))
+        if expr.evaluate(temp):
+            yield temp
+#frozenset I used to make the set immutable so that it can be used only as a 
+#key for the dictary and not be able to be changed at all after it is created.
 class KnowledgeBase(object):
     def __init__(self):
-        pass
+        self.fact = set()
+
     def get_facts(self):
-        pass
+        return set(self.fact)
+    
     def tell(self, expr):
-        pass
+        temp_expr_cnf = expr.to_cnf()
+        if isinstance(temp_expr_cnf, And):
+            self.fact.update(temp_expr_cnf.conjuncts)
+        else:
+            self.fact.add(temp_expr_cnf)
+    
+    # the ask function is used to check if the expr is true or false
+    # for the knowledge base and it uses the resolution which was said in the hw and 
+    # I think the it is best way to check the expr. 
     def ask(self, expr):
-        pass
+        def check_set(clause):
+            stack, listof_lit = [clause], set()
+            while stack:
+                item = stack.pop()
+                if isinstance(item, Or):
+                    stack.extend(item.disjuncts)
+                #if the item is not a disjun, we need to check if it is an atom or not.
+                elif isinstance(item, Atom) or (isinstance(item, Not) and isinstance(item.arg, Atom)):
+                    listof_lit.add(item)
+            return frozenset(listof_lit)
+
+        clause = {check_set(a) for a in self.fact}
+        neg = Not(expr).to_cnf()
+        if isinstance(neg, And):
+            clause.update({check_set(a) for a in neg.conjuncts})
+        else:
+            clause.add(check_set(neg))
+        # I used the resolution algorithm determine entailment.
+        while True:
+            new = set()
+            clist = list(clause)
+            for i, clause_one in enumerate(clist):
+                #if we have a clause that is empty then we can return true.
+                #otherwise we need keep going and checking from 1 to end.
+                for clause_two in clist[i + 1:]:
+                    for lit in clause_one:
+                        comp = Not(lit) if isinstance(lit, Atom) else lit.arg
+                        if comp in clause_two:
+                            #the clause_one and clause_two are the clauses that are checking
+                            #if they are the same or not and if they are then we just return T.
+                            resolvent = (clause_one - {lit}) | (clause_two - {comp})
+                            if not resolvent:
+                                return True
+                            #if it is resolvent then we need to add it to the new set since we can't change the frozenset.
+                            new.add(frozenset(resolvent))
+
+            #small one last check to see if the new set is a subset of the clause set.
+            if new.issubset(clause):
+                return False
+            clause.update(new)
 
 ############################################################
 # Section 2: Logic Puzzles
